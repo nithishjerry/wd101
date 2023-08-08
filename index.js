522ddb47153306e608a65ce61b2db89dcb52aa7a
nithishjerry <141728016+nithishjerry@users.noreply.github.com>
2023-08-08 15:49:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const startEntries = () => {
+    let entries = localStorage.getItem("user-Entries");
+    return entries ? JSON.parse(entries) : [];
+  };
+  
+  let userEntries = startEntries();
+  
+  const executeEntries = () => {
+    const entries = startEntries();
+  
+    const tableEntries = entries
+        .map(
+            (entry) => `<tr>
+                <td style = "padding: 2px 4px">${entry.name}</td>
+                <td style = "padding: 2px 4px">${entry.email}</td>
+                <td style = "padding: 2px 4px">${entry.password}</td>
+                <td style = "padding: 2px 4px">${entry.dob}</td>
+                <td style = "padding: 2px 4px">${entry.acceptedTermsAndConditions}</td>
+            </tr>`
+        )
+        .join("\n");
+  
+    const table = `<table>
+        
+        <tbody>${tableEntries}</tbody>
+    </table>`;
+  
+    let details = document.getElementById("user-entries");
+    details.innerHTML = table;
+  };
+  
+  const saveForm = (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTermsAndConditions = document.getElementById("Accepted term").checked;
+  
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndConditions,
+    };
+  
+    userEntries.push(entry);
+    localStorage.setItem("user-Entries", JSON.stringify(userEntries));
+    executeEntries();
+    userForm.reset();
+  };
+  
+  let userForm = document.getElementById("form");
+  userForm.addEventListener("submit", saveForm);
+  
+  executeEntries();
